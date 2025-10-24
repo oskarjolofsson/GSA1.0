6ad1357b91d22eb8965e6f05b939040978e64527
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-10-24 12:14:43 +0200

    logging users drills

--- backend/services/firebase/firebase_past_analysis.py
+++ backend/services/firebase/firebase_past_analysis.py
@@ -4,26 +4,25 @@ from services.firebase.firebase import FireBaseService
 
 
 class FireBasePastAnalysis(FireBaseService):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, user_id: str):
+        super().__init__(user_id=user_id)
 
-    def log_past_drills(self, user_id, drill_data):
+    def log_past_drills(self, drill_data: list[str]) -> None:
         """
         Log past drill data for a user
         Args:
-            user_id: The Firebase user ID
-            drill_data: A dictionary containing drill details
-            
+            drill_data: A list of drill data to log, String
         Returns:
-            bool: True if logging was successful, False otherwise
+            None
         """
         if not self.db:
             raise Exception("Firestore not initialized")
     
-        analysis_ref = self.db.collection('users').document(user_id).collection('past_drills')
-        analysis_ref.add(drill_data)
+        analysis_ref = self.db.collection('users').document(self.user_id).collection('past_drills')
+        for drill in drill_data:
+            analysis_ref.add(drill)
 
-    def get_past_drills(self, user_id):
+    def get_past_drills(self) -> list[dict]:
         """
         Retrieve past drills for a user
         
@@ -36,10 +35,5 @@ class FireBasePastAnalysis(FireBaseService):
         if not self.db:
             raise Exception("Firestore not initialized")
 
-        analysis_ref = self.db.collection('users').document(user_id).collection('past_drills')
+        analysis_ref = self.db.collection('users').document(self.user_id).collection('past_drills')
         return [doc.to_dict() for doc in analysis_ref.stream()] or []
-        
-        
-
-# Create a singleton instance
-firebase_past_analysis = FireBasePastAnalysis()
