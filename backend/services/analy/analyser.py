b951e9372e7615e8663288720cdf7ad391806edf
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-10-24 12:05:11 +0200

    spend tokens in backend instead

--- backend/services/analy/analyser.py
+++ backend/services/analy/analyser.py
@@ -1,5 +1,6 @@
 from services.analy.Sports.sportInstructions import SportAnalysis
 from services.analy.Models.model import Model
+from services.firebase.firebase_tokens import FireBaseTokens
 
 # Add models and sports as needed
 # Import models and sports analyses
@@ -37,13 +38,24 @@ class Analysis():
                 video_FileStorage, 
                 prompt: str = "", 
                 start_time: float = None, 
-                end_time: float = None):
+                end_time: float = None, 
+                user_id: str = "") -> dict:
+        
+        # Check current balance first
+        current_balance = FireBaseTokens(user_id).get_user_tokens()
+        if current_balance < 1:
+            raise ValueError("Insufficient tokens to perform analysis.")
 
+        # Create analysis
         sport_class = self.__get_sport_analysis(sport_name)
         sport_analysis = sport_class()
         
         model_class = self.__get_model(model_name)
         model_instance = model_class(system_instructions=sport_analysis)
         
-        return_dict = model_instance.get_response(video_FileStorage, prompt, start_time, end_time)
+        return_dict = model_instance.get_response(video_FileStorage=video_FileStorage, 
+                                                  prompt=prompt, 
+                                                  start_time=start_time, 
+                                                  end_time=end_time, 
+                                                  user_id=user_id)
         return return_dict
