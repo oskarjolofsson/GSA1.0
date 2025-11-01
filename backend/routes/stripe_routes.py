39ee2c8893cd22eb6cd3c51783e5c5d457d7515a
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-11-01 22:45:06 +0100

    webhook events added

--- backend/routes/stripe_routes.py
+++ backend/routes/stripe_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from backend.services.stripe.handleSubscriptionDeleted import HandleSubscriptionDeleted
+from services.stripe.handleSubscriptionDeleted import HandleSubscriptionDeleted
 from services.firebase.firebase_auth import require_auth
 from services.firebase.firebase_stripe import FirebaseStripeService
 from services.stripe.stripeSession import StripeSessionService
@@ -7,7 +7,7 @@ from services.stripe.stripeWebhook import StripeWebhookService
 
 
 # Create a Blueprint for stripe routes
-stripe_bp = Blueprint('stripe', __name__, url_prefix='/api/v1/stripe')
+stripe_bp = Blueprint('stripe', __name__, url_prefix='/stripe')
 
 @stripe_bp.route('/create-checkout-session', methods=['POST'])
 @require_auth       
@@ -23,10 +23,11 @@ def create_checkout_session():
         return jsonify({"error": "Missing priceId"}), 400
 
     uid = request.user["uid"]
+    email = request.user.get("email")
     
     try:
-        session = StripeSessionService(customer_id=FirebaseStripeService(uid).get_or_create_customer(), 
-                                    price_id=price_id, 
+        session = StripeSessionService(customer_id=FirebaseStripeService(uid, email).get_or_create_customer(),
+                                    price_id=price_id,
                                     firebase_uid=uid).create_checkout_session()
         
         return jsonify({"url": session.url}), 200
@@ -37,8 +38,9 @@ def create_checkout_session():
             'details': str(e)
         }), 500
 
-@stripe_bp.route('/stripe/webhook', methods=['POST'])
+@stripe_bp.route('/webhook', methods=['POST'])
 def webhook():
+    print("Received Stripe webhook")
     service = StripeWebhookService(
         sig_header=request.headers.get("Stripe-Signature"),
         payload=request.data
@@ -50,8 +52,10 @@ def webhook():
 @require_auth
 def create_portal_session():
     try:
-        url = (StripeSessionService(firebase_uid=request.user["uid"], 
-                                customer_id=FirebaseStripeService(request.user["uid"]).get_or_create_customer())
+        uid = request.user["uid"]        
+        email = request.user.get("email")
+        url = (StripeSessionService(firebase_uid=uid,
+                                customer_id=FirebaseStripeService(firebase_user_id=uid, email=email).get_or_create_customer())
                 .create_portal_session())
 
         return jsonify({"url": url})
@@ -69,22 +73,24 @@ def subscription_status():
     Get the subscription status of the current user.
     """
     uid = request.user["uid"]
+    email = request.user.get("email")
     
     if not uid:
         return jsonify({"status": "Inactive"}), 200
-    
-    user_data = FirebaseStripeService(uid).db_get_user(uid)
+
+    user_data = FirebaseStripeService(uid, email=email).db_get_user()
     status = user_data.get("status", "free")
     price_id = user_data.get("stripe_price_id", None)
-    return jsonify({"price_id": price_id}), 200
+    return jsonify({"price_id": price_id, "status": status}), 200
 
 @stripe_bp.post("/cancel-subscription")
 @require_auth
 def cancel_subscription():
     try:
         uid = request.user["uid"]
-        user = FirebaseStripeService(uid).db_get_user()
-        
+        email = request.user.get("email")
+        user = FirebaseStripeService(uid, email=email).db_get_user()
+
         canceled_sub = HandleSubscriptionDeleted(
             customer_id=user.get("stripe_customer_id"),
             subscription_id=user.get("stripe_subscription_id")
