a76df1f5cb7f556ef6fa333d2af2e04f758e8b13
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-11-01 16:25:07 +0100

    saving user info on login

---
+++ backend/routes/user_routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request, jsonify
+from services.firebase.firebase_auth import require_auth
+from services.firebase.firebase_tokens import FireBaseTokens
+from services.firebase.firebase_stripe import FirebaseStripeService
+
+
+# Create the blueprint
+user_bp = Blueprint('users', __name__, url_prefix='/api/v1/users')
+
+@user_bp.route('/register', methods=['POST']) 
+@require_auth
+def register_user():
+    try:
+        uid = request.user["uid"]
+        FireBaseTokens(user_id=uid).initialize_user_tokens()
+        FirebaseStripeService(firebase_user_id=uid).get_or_create_customer()
+
+        return jsonify({
+            "success": True,
+        }), 201
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error": str(e)
+        }), 500
+
+
