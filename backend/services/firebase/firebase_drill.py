55bf110d91829d859a4f925e1064510637611914
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-01-10 22:38:06 +0100

    table for drills

---
+++ backend/services/firebase/firebase_drill.py
@@ -0,0 +1,68 @@
+from services.firebase.firebase import FireBaseService
+from firebase_admin import firestore
+from uuid import uuid4
+
+class FirebaseDrillService(FireBaseService):
+    
+    def __init__(self, user_id: str):
+        super().__init__(user_id=user_id)
+        self.drills_ref = self.db.collection('drills')
+        
+
+    def add_drill(self, drill: str) -> str:
+        """
+        Adds a new drill document to the 'drills' collection.
+
+        Args:
+            drill (str): The drill content to be stored.
+
+        Returns:
+            str: The ID of the newly created drill document.
+        """
+        drill_id = "drill_" + str(uuid4())
+        
+        document_data = {
+            "user_id": self.user_id,
+            "drill": drill,
+            "drill_id": drill_id,
+            "image_url": None,
+        }
+        
+        self.drills_ref.document(drill_id).set(document_data)
+        
+        return drill_id
+    
+    def update_drill_image(self, drill_id: str, image_url: str) -> None:
+        """
+        Updates the image URL of an existing drill document.
+
+        Args:
+            drill_id (str): The ID of the drill document to be updated.
+            image_url (str): The new image URL to be set.
+        """
+        drill_doc_ref = self.drills_ref.document(drill_id)
+        
+        drill_doc_ref.update({
+            "image_url": image_url
+        })
+        
+    def extract_drill_from_analysis(self, analysis: dict) -> dict:
+        """
+        Extracts a drill from analysis data, returns analysis added with drill ID.
+
+        Args:
+            analysis (dict): The analysis data containing the drill information.
+
+        Returns:
+            dict: Analysis data with the drill ID included.
+        """
+        key_findings = analysis.get("analysis_results", {}).get("key_findings", [])
+        
+        for finding in key_findings:
+            drill_text = finding.get("try_this", "")
+            if drill_text:
+                drill_id = self.add_drill(drill_text)
+                finding["drill_id"] = drill_id
+                
+        return analysis
+                
