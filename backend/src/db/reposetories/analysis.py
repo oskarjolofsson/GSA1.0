717c0810f3c44e024d2513c411e2634c120a4da8
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-02-01 22:24:33 +0100

    implment repo-methods for analyses

--- backend/src/db/reposetories/analysis.py
+++ backend/src/db/reposetories/analysis.py
@@ -1,8 +1,45 @@
-from sqlalchemy import text
+from sqlalchemy import select, text
 from ..session import SessionLocal
 from ..models.Analysis import Analysis
 
 
-def get_analysis_by_id(analysis_id):
+def get_analysis_by_id(analysis_id) -> Analysis:
     with SessionLocal() as session:
         return session.get(Analysis, analysis_id)
+    
+    
+def get_analyses_by_user_id(user_id) -> list[Analysis]:
+    with SessionLocal() as session:
+        stmt = (
+            select(Analysis)
+            .where(Analysis.user_id == user_id)
+            .where(Analysis.status == text("'completed'"))
+            .where(Analysis.success == True)
+            .order_by(Analysis.created_at.desc())
+        )
+        
+        return session.scalars(stmt).all()
+    
+
+def create_analysis(analysis: Analysis) -> Analysis:
+    with SessionLocal() as session:
+        try:
+            session.add(analysis)
+            session.commit()
+            session.refresh(analysis)
+            return analysis
+        except Exception:
+            session.rollback()
+            raise
+
+
+def update_analysis(analysis: Analysis) -> Analysis:
+    with SessionLocal() as session:
+        try:
+            session.merge(analysis)
+            session.commit()
+            session.refresh(analysis)
+            return analysis
+        except Exception:
+            session.rollback()
+            raise
